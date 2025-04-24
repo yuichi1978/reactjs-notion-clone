881c0adc9089983f2c35a984e09018495828512d
yuichi1978 <jwpt62338@iris.eonet.ne.jp>
2025-04-24 16:15:34 +0900

    react varsion

--- src/components/TitleInput.tsx
+++ src/components/TitleInput.tsx
@@ -1,4 +1,4 @@
-import { Note } from "@/modules/notes/note";
+import { Note } from "@/modules/notes/note.entity";
 import { useState } from "react";
 import TextAreaAutoSize from "react-textarea-autosize";
 
